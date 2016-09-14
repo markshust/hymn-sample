ef04b7ad228ece366f67fb2ff0b7ec45c4ba5439
Mark Shust <mark@shust.com>
2016-09-13 21:31:27 -0400

    Consolidated routes into one file at core

--- imports/client/core/routes.js
+++ imports/client/core/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
-import homeRoutes from 'client/home/routes';
+import { browserHistory, IndexRoute, Router, Route } from 'react-router';
+import Home from 'client/home/components/Home';
 import App from './components/App';
 
 const routes = (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      {homeRoutes}
+      <IndexRoute component={Home} />
     </Route>
   </Router>
 );
--- imports/client/home/routes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { IndexRoute } from 'react-router';
-import Home from './components/Home';
-
-const routes = (
-  <IndexRoute component={Home} />
-);
-
-export default routes;
